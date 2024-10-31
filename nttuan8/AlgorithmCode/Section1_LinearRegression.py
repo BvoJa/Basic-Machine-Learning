6c3c695df0807ad61a0940124ba14e34e54dd5a5
BvoJa <24520006@gm.uit.edu.vn>
2024-10-31 09:01:37 +0700

    Add updated section 2

--- nttuan8/AlgorithmCode/Section1_LinearRegression.py
+++ nttuan8/AlgorithmCode/Section1_LinearRegression.py
@@ -6,6 +6,9 @@ data = pd.read_csv("D:\Download\dataset1.csv").values
 N = data.shape[0]
 X = data[:, 0].reshape(-1, 1)
 y = data[:, 1].reshape(-1, 1)
+plt.scatter(X, y, c = 'blue')
+plt.xlabel('Area')
+plt.ylabel('Price')
 
 X = np.hstack((np.ones((N, 1)), X))
 w = np.array([0., 1.0]).reshape(-1, 1)
@@ -14,13 +17,16 @@ learning_rate = 0.000001
 cost = np.zeros((numOfIteration, 1))
 
 for i in range(numOfIteration):
-    r = np.dot(X, w) - y
-    cost[i] = 0.5 * np.sum(r * r)
-    w -= learning_rate * np.dot(X.T, r)
+    y_predict = np.dot(X, w) - y
+    cost[i] = 0.5 * np.sum(y_predict * y_predict)
+    w -= learning_rate * np.dot(X.T, y_predict)
     
 predictedVal = np.dot(X, w)
-plt.scatter(data[:, 0], data[:, 1], c = 'blue')
 plt.plot((X[0][1], X[N - 1][1]), (predictedVal[0], predictedVal[N - 1]), c = 'red')
 plt.show()
 print(w)
+
+
+
+
 
--- nttuan8/AlgorithmCode/Section2_LogisticRegression.py
+++ nttuan8/AlgorithmCode/Section2_LogisticRegression.py
@@ -2,4 +2,33 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
+def Sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
 data = pd.read_csv("D:\Download\dataset2.csv").values
+row, col = data.shape
+X = data[:, 0 : col - 1].reshape(-1, col - 1)
+y = data[:, 2].reshape(-1, 1)
+plt.scatter(X[:10, 0], X[:10, 1], s = 15, c = 'green', label = 'Yes')
+plt.scatter(X[10:, 0], X[10:, 1], s = 15, c = 'red', label = 'No')
+plt.legend(loc = 1)
+plt.xlabel('Salary (Million)')
+plt.ylabel('Experience (Year)')
+
+X = np.hstack((np.ones((row, 1)), X))
+w = np.array([0., 0.1, 0.1]).reshape(-1, 1)
+numOfIteration = 1000
+learning_rate = 0.01
+cost = np.zeros((numOfIteration, 1))
+
+for i in range(numOfIteration):
+    y_predict = Sigmoid(np.dot(X, w))
+    cost[i] = -np.sum(np.multiply(y, np.log(y_predict)) + np.multiply(1 - y, np.log(1 - y_predict)))
+    w -= learning_rate * np.dot(X.T, y_predict - y)
+
+t = 0.5 
+plt.plot((4, 10), (-(w[0] + 4 * w[1] + np.log(1 / t - 1)) / w[2], -(w[0] + 10 * w[1]+ np.log(1 / t - 1)) / w[2]), 'blue')
+plt.show()
+
+
+
