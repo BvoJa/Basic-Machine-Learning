1a3bca36e5aa7f04e98e3f8bafb0113b037e9330
BvoJa <24520006@gm.uit.edu.vn>
2024-11-04 17:14:08 +0700

    Add DecisionnTree.py

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,4 +1,18 @@
 import pandas as pd
+import matplotlib.pyplot as plt 
+from sklearn import tree 
 
 data = pd.read_csv("D:\Download\DecisionTreedata.csv")
-print(data)
+num = {'UK' : 0, 'USA' : 1, 'N' : 1}
+data['Nationality'] = data['Nationality'].map(num)
+num = {'NO' : 0, 'YES' : 1}
+data['Go'] = data['Go'].map(num)
+
+features = ['Age', 'Experience', 'Rank', 'Nationality']
+X = data[features]
+y = data['Go']
+
+dtree = tree.DecisionTreeClassifier()
+dtree.fit(X, y)
+tree.plot_tree(dtree, feature_names = features)
+plt.show()
